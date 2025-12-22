339d9c001a98c32570d85ffe2c614b08074f82fb
古宮信二 <>
2025-12-22 19:18:29 +0900

    accountを登録できるようにして、データベースも変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,16 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  # Deviseのコントローラーが動くときに、名前を許可するメソッドを呼ぶ
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    # アカウント登録（sign_up）の際に、nameを保存できるように許可する
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    # アカウント更新（account_update）の際も必要なら追加
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
 end
