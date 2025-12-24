c643c8b5e8f36efa368c26372f3df7746be416f4
古宮信二 <>
2025-12-24 15:49:05 +0900

    JSの新規ファイルを作成して管理

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,14 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-
-document.addEventListener('turbo:load', () => {
-  const trigger = document.getElementById('avatar_trigger');
-  const input = document.getElementById('avatar_input');
-
-  if (trigger && input) {
-    trigger.addEventListener('click', () => {
-      input.click();
-    });
-  }
-});
+import "profile_image"
