6fd8a7b97a35734c88e69f323764740e98343191
古宮信二 <>
2025-12-27 18:17:46 +0900

    タイムラインの画像をプロフィール画像が表示できるように変更

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
 def index
     # データベースからすべての投稿を取得し、作成日順（新しい順）に並べて代入する
     @posts = Post.all.order(created_at: :desc)
+    @profile = current_user.prepare_profile
   end
 end
