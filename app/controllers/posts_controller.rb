edb24ffb50cd2fd12912b4b99aa8505d1cc57cc0
古宮信二 <>
2025-12-27 15:44:00 +0900

    postの新規投稿画面を表示できるように変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,23 @@
-def post_params
-  params.require(:post).permit(:content, images: [])
+class PostsController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to root_path, notice: "投稿しました！"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    # images: [] と書くことで、複数の画像を受け取れるようになります
+    params.require(:post).permit(:caption, :likes_count, images: [])
+  end
 end
